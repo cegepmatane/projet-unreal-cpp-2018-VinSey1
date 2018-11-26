3243151ec57117b256b3a47d48d1d02028807332
Vincent Seyller <vseyller@laposte.net>
2018-11-26 01:41:18 -0500

    Ajout fonction exposée canard : zone de détection

--- Private/Canard.cpp
+++ Private/Canard.cpp
@@ -1,7 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Canard.h"
+#include "Components/StaticMeshComponent.h"
 
+ACanard::ACanard() {
+	this->getCorpsEnnemi()->SetSimulatePhysics(true);
+}
 
-
-
+int ACanard::getZoneDeDetection() {
+	return this->zoneDeDetection;
+}
--- Public/Canard.h
+++ Public/Canard.h
@@ -14,7 +14,12 @@ class CHASSEMANGER_API ACanard : public AEnnemi
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	ACanard();
+	UFUNCTION(BlueprintPure, Category = "Etat")
+		int getZoneDeDetection();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Options", meta = (AllowPrivateAccess = "true"))
+		int zoneDeDetection;
 };
