7c4b0b93311c58beb991c8eb9744d8da696ccacd
Vincent Seyller <vseyller@laposte.net>
2018-11-26 00:46:15 -0500

    Création des sous classes d'ennemis : Chien, Chat et Canard

---
+++ Public/Canard.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ennemi.h"
+#include "Canard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHASSEMANGER_API ACanard : public AEnnemi
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Public/Chien.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ennemi.h"
+#include "Chien.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHASSEMANGER_API AChien : public AEnnemi
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
