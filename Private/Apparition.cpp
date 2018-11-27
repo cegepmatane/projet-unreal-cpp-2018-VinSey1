f12611275792d83adbed50e7c09bbb8dc79913e7
Vincent Seyller <vseyller@laposte.net>
2018-11-27 14:49:14 -0500

    Création classe Apparition

---
+++ Private/Apparition.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Apparition.h"
+
+
+// Sets default values
+AApparition::AApparition()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AApparition::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AApparition::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
